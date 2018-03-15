6c60561f80a13d46e40ff6fa811459698ea348dd
Junior <junior2124@msn.com>
2018-03-15 14:22:01 -0500

    FirstCommit

---
+++ Calculator/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Calculator;
+public partial class _Default : System.Web.UI.Page
+{
+    CalculatorService oCalc = new CalculatorService();
+ 
+    decimal a, b, c;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        a = Convert.ToDecimal(txtFno.Text);
+        b = Convert.ToDecimal(txtSno.Text);
+        c = oCalc.Add(a, b);
+        lblResult.Text = c.ToString();
+    }
+    protected void btnSub_Click(object sender, EventArgs e)
+    {
+        a = Convert.ToDecimal(txtFno.Text);
+        b = Convert.ToDecimal(txtSno.Text);
+        c = oCalc.Sub(a, b);
+        lblResult.Text = c.ToString();
+    }
+    protected void BtnMul_Click(object sender, EventArgs e)
+    {
+        a = Convert.ToDecimal(txtFno.Text);
+        b = Convert.ToDecimal(txtSno.Text);
+        c = oCalc.Mul(a, b);
+        lblResult.Text = c.ToString();
+    }
+    protected void btnDiv_Click(object sender, EventArgs e)
+    {
+
+        a = Convert.ToDecimal(txtFno.Text);
+        b = Convert.ToDecimal(txtSno.Text);
+        c = oCalc.Div(a, b);
+        lblResult.Text = c.ToString();
+    }
+}
---
+++ WebServiceExample/App_Code/CalculatorService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for CalculatorService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class CalculatorService : System.Web.Services.WebService
+{
+
+    public CalculatorService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public decimal Add(decimal x, decimal y)
+    {
+        return Math.Round(x + y, 2);
+    }
+    [WebMethod]
+    public decimal Sub(decimal x, decimal y)
+    {
+        return Math.Round(x - y, 2);
+    }
+    [WebMethod]
+    public decimal Mul(decimal x, decimal y)
+    {
+        return Math.Round(x * y, 2);
+    }
+    [WebMethod]
+    public decimal Div(decimal x, decimal y)
+    {
+        return Math.Round(x / y, 2);
+    }
+
+}
